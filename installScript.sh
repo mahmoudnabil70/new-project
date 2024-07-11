d096755330edf14ad17fe636358d516f532cde9e
mahmoudnabil70 <mahmoud.nabil.moselhy@gmail.com>
2024-07-11 05:32:07 +0300

    update jenkins install

--- installScript.sh
+++ installScript.sh
@@ -5,11 +5,14 @@ sudo apt upgrade -y
 sudo apt-get install openjdk-11-jdk -y
 #Install Jenkins 
 echo "--------------------Installing Jenkins--------------------"
-sudo apt -y install wget
-sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update -y
-sudo apt-get install jenkins -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-17-jre
+sudo apt-get install jenkins
 sudo apt install git
 sudo systemctl start jenkins
 #Install docker
